7808eae932e549c3508e2221145f92086067da55
wcp91 <1475140329@qq.com>
2020-09-10 18:25:04 +0800

    add

--- scripts/prefabs/coffee.lua
+++ scripts/prefabs/coffee.lua
@@ -6,7 +6,7 @@ local assets =
 
 local prefabs =
 {
-    "coffee_cooked",
+    "coffeebean_cooked",
     "spoiled_food",
 }
 
@@ -134,5 +134,83 @@ local function fn_cooked()
     return inst
 end
 
-return Prefab("coffee", fn, assets, prefabs),
-    Prefab("coffee_cooked", fn_cooked, assets, prefabs)
+local function EatCoffeeFn(inst, eater)
+
+end
+
+local function fn_prepared()
+    local inst = CreateEntity()
+
+    inst.entity:AddTransform()
+    inst.entity:AddAnimState()
+    inst.entity:AddNetwork()
+
+    MakeInventoryPhysics(inst)
+
+    inst.AnimState:SetBuild("coffee")
+    inst.AnimState:SetBank("coffee")
+    
+
+    inst.AnimState:PlayAnimation("idle")
+    --inst.AnimState:OverrideSymbol("swap_food", data.overridebuild or "cook_pot_food", data.basename or data.name)
+
+    inst:AddTag("preparedfood")
+
+    MakeInventoryFloatable(inst)
+
+    inst.entity:SetPristine()
+
+    if not TheWorld.ismastersim then
+        return inst
+    end
+
+    inst:AddComponent("edible")
+    inst.components.edible.healthvalue = 3
+    inst.components.edible.hungervalue = 10
+    inst.components.edible.foodtype = FOODTYPE.GENERIC
+    inst.components.edible.secondaryfoodtype = nil
+    inst.components.edible.sanityvalue = -5
+    inst.components.edible.temperaturedelta = 0
+    inst.components.edible.temperatureduration = 0
+    inst.components.edible.nochill = nil
+    --inst.components.edible.spice = nil
+    inst.components.edible:SetOnEatenFn(EatCoffeeFn)
+
+    inst:AddComponent("inspectable")
+    inst.wet_prefix = data.wet_prefix
+
+    inst:AddComponent("inventoryitem")
+
+    if spicename ~= nil then
+        inst.components.inventoryitem:ChangeImageName(spicename.."_over")
+    elseif data.basename ~= nil then
+        inst.components.inventoryitem:ChangeImageName(data.basename)
+    end
+
+    inst:AddComponent("stackable")
+    inst.components.stackable.maxsize = TUNING.STACK_SIZE_SMALLITEM
+
+    if data.perishtime ~= nil and data.perishtime > 0 then
+        inst:AddComponent("perishable")
+        inst.components.perishable:SetPerishTime(data.perishtime)
+        inst.components.perishable:StartPerishing()
+        inst.components.perishable.onperishreplacement = "spoiled_food"
+    end
+
+    MakeSmallBurnable(inst)
+    MakeSmallPropagator(inst)
+    MakeHauntableLaunchAndPerish(inst)
+    ---------------------
+
+    inst:AddComponent("bait")
+
+    ------------------------------------------------
+    inst:AddComponent("tradable")
+
+    ------------------------------------------------
+
+    return inst
+end
+
+return Prefab("coffeebean", fn, assets, prefabs),
+    Prefab("coffeebean_cooked", fn_cooked, assets, prefabs)
