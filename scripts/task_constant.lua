e0ed190637236e0538f144b27549182cf3c8b649
wcp91 <1475140329@qq.com>
2020-10-16 18:00:44 +0800

    tj

--- scripts/task_constant.lua
+++ scripts/task_constant.lua
@@ -26,7 +26,7 @@ task_data = {
 		reward=2
 	},
 	pick_tumbleweed888 = {
-		name="喜奔小康",
+		name="习以为常",
 		desc="采集风滚草888次",
 		need=888,
 		reward=3
@@ -44,8 +44,51 @@ task_data = {
 		reward=6,
 		hide=true
 	},
+	pick_tumbleweed_red_100 = {
+		name="好运气",
+		desc="采集红色风滚草100次",
+		need=100,
+		reward=3
+	},
+	pick_tumbleweed_yellow_60 = {
+		name="发现宝藏",
+		desc="采集橙色风滚草60次",
+		need=60,
+		reward=4
+	},
+	pick_tumbleweed_light_20 = {
+		name="神秘宝物",
+		desc="采集橙色风滚草60次",
+		need=20,
+		reward=5,
+		hide=true
+	},
+	pick_tumbleweed_green_120 = {
+		name="灾厄之物",
+		desc="采集橙色风滚草120次",
+		need=120,
+		reward=3
+	},
+	pick_tumbleweed_blue_60 = {
+		name="厄运之兆",
+		desc="采集橙色风滚草60次",
+		need=60,
+		reward=4
+	},
 
 	--食物部分
+	eat100 = {
+		name="吃饱",
+		desc="吃食物100次",
+		need=100,
+		reward=1
+	},
+	eat1888 = {
+		name="饱荒",
+		desc="吃食物1888次",
+		need=1888,
+		reward=1
+	},
 
 	--击杀部分
 
