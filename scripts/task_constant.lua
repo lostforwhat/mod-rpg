65e90678155cd803c49ba5eb6f68659a080f1b6e
wcp91 <1475140329@qq.com>
2020-10-26 18:31:35 +0800

    add

--- scripts/task_constant.lua
+++ scripts/task_constant.lua
@@ -163,10 +163,297 @@ task_data = {
 		need=100,
 		reward=2
 	},
-	
+	kill_koale_5 = {
+		name="征服大象",
+		desc="击杀5只大象",
+		need=5,
+		reward=2
+	},
+	kill_monkey_20 = {
+		name="戏耍猴子",
+		desc="击杀20只猴子",
+		need=20,
+		reward=1
+	},
+	kill_leif_5 = {
+		name="行走的树",
+		desc="击杀5个树人",
+		need=5,
+		reward=2
+	},
+	kill_bunnyman_20 = {
+		name="不干净",
+		desc="击杀20个兔人",
+		need=20,
+		reward=2
+	},
+	kill_tallbird_50 = {
+		name="追人的高鸟",
+		desc="击杀50只高脚鸟",
+		need=50,
+		reward=3
+	},
+	kill_worm_20 = {
+		name="会动的植物",
+		desc="击杀20只蠕虫",
+		need=20,
+		reward=2
+	},
+	kill_slurtle_20 = {
+		name="致命蜗牛",
+		desc="击杀20只蜗牛",
+		need=20,
+		reward=2
+	},
+	kill_rabbit_10 = {
+		name="小兔子乖乖",
+		desc="击杀10只小兔子",
+		need=10,
+		reward=1
+	},
+	kill_ghost_10 = {
+		name="我不怕鬼",
+		desc="击杀10个鬼魂",
+		need=10,
+		reward=1
+	},
+	kill_tentacle_50 = {
+		name="小心脚下",
+		desc="击杀50根触手",
+		need=50,
+		reward=2
+	},
+	kill_terrorbeak_50 = {
+		name="消除噩梦",
+		desc="击杀50只梦魇",
+		need=50,
+		reward=2
+	},
+	kill_birchnutdrake_20 = {
+		name="坚果成精",
+		desc="击杀20只桦树坚果",
+		need=20,
+		reward=2
+	},
+	kill_lightninggoat_20 = {
+		name="带电的羊",
+		desc="击杀20只电羊",
+		need=20,
+		reward=2
+	},
+	kill_spiderqueen_10 = {
+		name="蜘蛛首领",
+		desc="击杀10只蜘蛛女王",
+		need=10,
+		reward=3
+	},
+	kill_warg_5 = {
+		name="猎犬首领",
+		desc="击杀5只狗王",
+		need=5,
+		reward=3
+	},
+	kill_catcoon_20 = {
+		name="喵喵喵",
+		desc="击杀20只浣熊猫",
+		need=20,
+		reward=2
+	},
+	kill_walrus_20 = {
+		name="喵喵喵",
+		desc="击杀20只浣熊猫",
+		need=20,
+		reward=2
+	},
+	kill_slurtle_20 = {
+		name="海象爸爸",
+		desc="击杀20只海象",
+		need=20,
+		reward=2
+	},
+	kill_butterfly_20 = {
+		name="黄油诅咒",
+		desc="击杀20只蝴蝶",
+		need=20,
+		reward=1
+	},
+	kill_bat_20 = {
+		name="吸血蝙蝠",
+		desc="击杀20只蝙蝠",
+		need=20,
+		reward=2
+	},
+	kill_merm_30 = {
+		name="浪哩个波",
+		desc="击杀30只鱼人",
+		need=30,
+		reward=2
+	},
+	kill_penguin_10 = {
+		name="冬季过客",
+		desc="击杀10只企鹅",
+		need=10,
+		reward=2
+	},
+	kill_perd_20 = {
+		name="今晚吃鸡",
+		desc="击杀20只火鸡",
+		need=20,
+		reward=2
+	},
+	kill_bird_20 = {
+		name="捕鸟人",
+		desc="击杀20只鸟",
+		need=20,
+		reward=2
+	},
+	kill_pigman_20 = {
+		name="你是个好人",
+		desc="击杀20只猪人",
+		need=20,
+		reward=2
+	},
+	kill_krampus_30 = {
+		name="包包留下",
+		desc="击杀30个坎普斯",
+		need=30,
+		reward=3
+	},
+	kill_spat = {
+		name="挑战刚羊",
+		desc="击杀刚羊",
+		need=1,
+		reward=2
+	},
+	kill_moonpig_10 = {
+		name="月下起武",
+		desc="满月击杀10只疯猪",
+		need=10,
+		reward=2
+	},
+	--boss
+	kill_moose = {
+		name="挑战鹿鸭",
+		desc="击杀鹿鸭",
+		need=1,
+		reward=5
+	},
+	kill_dragonfly = {
+		name="挑战龙蝇",
+		desc="击杀龙蝇",
+		need=1,
+		reward=5
+	},
+	kill_beager = {
+		name="挑战熊獾",
+		desc="击杀熊獾",
+		need=1,
+		reward=5
+	},
+	kill_deerclops = {
+		name="挑战巨鹿",
+		desc="击杀巨鹿",
+		need=1,
+		reward=5
+	},
+	kill_killshadow_3 = {
+		name="暗影三基佬",
+		desc="击杀暗影主教，暗影战车，暗影骑士",
+		need=3,
+		reward=5
+	},
+	kill_stalker = {
+		name="挑战远古狩猎者",
+		desc="击杀远古狩猎者",
+		need=1,
+		reward=5
+	},
+	kill_stalker_atrium = {
+		name="挑战远古暗影编织者",
+		desc="击杀远古暗影编织者",
+		need=1,
+		reward=8
+	},
+	kill_klaus = {
+		name="挑战克劳斯",
+		desc="击杀克劳斯",
+		need=1,
+		reward=8
+	},
+	kill_antlion = {
+		name="挑战蚁狮",
+		desc="击杀蚁狮",
+		need=1,
+		reward=5
+	},
+	kill_minotaur = {
+		name="挑战远古守护者",
+		desc="击杀远古守护者",
+		need=1,
+		reward=6
+	},
+	kill_beequeen = {
+		name="挑战蜜蜂女王",
+		desc="击杀蜜蜂女王",
+		need=1,
+		reward=6
+	},
+	kill_toadstool = {
+		name="挑战毒菌蘑菇",
+		desc="击杀毒菌蘑菇",
+		need=1,
+		reward=6
+	},
+	kill_toadstool_dark = {
+		name="挑战暗黑毒菌蘑菇",
+		desc="击杀暗黑毒菌蘑菇",
+		need=1,
+		reward=8
+	},
+	kill_malbatross = {
+		name="挑战邪天翁",
+		desc="击杀邪天翁",
+		need=1,
+		reward=5
+	},
+	kill_crabking = {
+		name="挑战帝王蟹",
+		desc="击杀帝王蟹",
+		need=1,
+		reward=5
+	},
 
 	--伤害部分
-
+	attack_30000 = {
+		name="战斗",
+		desc="总共造成有效伤害30000",
+		need=30000,
+		reward=2
+	},
+	hurt_10000 = {
+		name="虚弱",
+		desc="总共承受有效伤害10000",
+		need=10000,
+		reward=2
+	},
+	damage_1 = {
+		name="手无缚鸡之力",
+		desc="单次造成伤害1点",
+		need=1,
+		reward=1
+	},
+	hurt_1 = {
+		name="不痛不痒",
+		desc="单次受到伤害1点",
+		need=1,
+		reward=1
+	},
+	damage_66 = {
+		name="计算精准",
+		desc="单次造成伤害66点",
+		need=1,
+		reward=1
+	},
 
 	--种植收割部分
 
