43026d64648f8cf1472c7b5cb1e5b1bb611b3193
lostforwhat <Wcp910911!>
2020-11-09 22:41:56 +0800

    add playerinfo

--- scripts/widgets/playerdetail.lua
+++ scripts/widgets/playerdetail.lua
@@ -7,6 +7,7 @@ local UIAnim = require "widgets/uianim"
 local Widget = require "widgets/widget"
 local ImageButton = require "widgets/imagebutton"
 local TEMPLATES = require "widgets/templates"
+local TEMPLATES2 = require "widgets/redux/templates"
 local EquipSlot = require("equipslotutil")
 
 local PlayerDetail = Class(Widget, function(self, owner)
@@ -25,6 +26,26 @@ local PlayerDetail = Class(Widget, function(self, owner)
     
 end)
 
+function PlayerDetail:SetPlayerData()
+    local options = {
+        { text = "个人信息", data = 1 },
+        { text = "称号", data = 2 }
+    }
+    self.top_nav = self.proot:AddChild(TEMPLATES.LabelSpinner("", options, 0, 160, 50, 20, NEWFONT, 30, -10))
+    self.top_nav:SetPosition(0, 268)
+    self.top_nav.spinner:SetTextColour(1,0.4,0.35,1)
+    self.top_nav.spinner:SetOnChangedFn(function(selected, old) 
+        if selected == 1 then
+            self.top_nav.spinner:SetTextColour(1,0.4,0.35,1)
+        else
+            self.top_nav.spinner:SetTextColour(0.5,0.35,0.18,1)
+        end
+    end)
+    
+
+
+end
+
 function PlayerDetail:Layout()
     self.frame = self.proot:AddChild(TEMPLATES.CurlyWindow(130, 540, .6, .6, 39, -25))
     self.frame:SetPosition(0, 20)
@@ -40,6 +61,8 @@ function PlayerDetail:Layout()
     self.in_pos = Vector3(-.95 * w, 0, 0)
 
     self:MoveTo(self.out_pos, self.in_pos, .33, function()  end)
+
+    self:SetPlayerData()
 
     self.close_button = self.proot:AddChild(TEMPLATES.SmallButton(STRINGS.UI.PLAYER_AVATAR.CLOSE, 26, .5, function() 
         self.owner.HUD:ClosePlayerDetail()
